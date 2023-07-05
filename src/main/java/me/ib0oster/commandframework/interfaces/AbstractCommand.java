0b16e51939774e39d07440376c047d3622e73854
Booster <87100981+ib0oster@users.noreply.github.com>
2023-07-05 15:02:37 -0300

    1.0.1
    Added AbstractCommand utils

--- src/main/java/me/ib0oster/commandframework/interfaces/AbstractCommand.java
+++ src/main/java/me/ib0oster/commandframework/interfaces/AbstractCommand.java
@@ -1,9 +1,20 @@
 package me.ib0oster.commandframework.interfaces;
 
+import discord4j.common.util.Snowflake;
+import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.entity.Guild;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.Role;
+import discord4j.core.object.entity.User;
+import discord4j.core.object.entity.channel.TextChannel;
+import discord4j.core.spec.EmbedCreateSpec;
 import discord4j.discordjson.json.ApplicationCommandOptionData;
+import discord4j.rest.util.Permission;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @SuppressWarnings("unused") @Getter @Setter
 public abstract class AbstractCommand {
@@ -11,5 +22,43 @@ public abstract class AbstractCommand {
     private String name;
 
     public abstract Object perform(ChatInputInteractionEvent event);
+
+    protected boolean hasPermission(Member member, Permission permission) {
+        assert false;
+        return member.getBasePermissions().block().contains(permission);
+    }
+
+    protected EmbedCreateSpec.Builder embedBuilder() {
+        return EmbedCreateSpec.builder();
+    }
+
+    protected boolean hasRole(Member member, Role role) {
+        assert false;
+        return member.getRoles().any(role::equals).block();
+    }
+
+    protected User getUserByID(Guild guild, long id) {
+        return guild.getClient().getUserById(Snowflake.of(id)).block();
+    }
+
+    protected TextChannel getChannelByID(Guild guild, long id) {
+        return guild.getChannelById(Snowflake.of(id)).ofType(TextChannel.class).block();
+    }
+
+    protected List<Member> getMembersWithRole(Guild guild, Role role) {
+        return guild.getMembers()
+                .filter(member -> member.getRoles().any(role::equals).blockOptional().orElse(false))
+                .collectList()
+                .block();
+    }
+
+    protected boolean isNumber(String text) {
+        try {
+            Integer.parseInt(text);
+            return true;
+        } catch (NumberFormatException t) {
+            return false;
+        }
+    }
 
 }
